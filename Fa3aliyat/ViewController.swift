76055cde031d0285f9d2896e7423c085fe9394aa
BoMaith <elyas29290@gmail.com>
2024-12-04 15:29:47 +0300

    bidaya

---
+++ Fa3aliyat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fa3aliyat
+//
+//  Created by BP-36-201-17 on 26/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fa3aliyatUITests/Fa3aliyatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fa3aliyatUITestsLaunchTests.swift
+//  Fa3aliyatUITests
+//
+//  Created by BP-36-201-17 on 26/11/2024.
+//
+
+import XCTest
+
+final class Fa3aliyatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
