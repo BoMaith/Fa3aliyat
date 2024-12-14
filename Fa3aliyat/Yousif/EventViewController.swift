680d6290e75df4c7b2509012dfe4df647ad2a917
yassfee <yassfee923@gmail.com>
2024-12-14 19:05:44 +0300

    nice

--- Fa3aliyat/Yousif/EventViewController.swift
+++ Fa3aliyat/Yousif/EventViewController.swift
@@ -1,4 +1,4 @@
-//
+	//
 //  EventViewController.swift
 //  Fa3aliyat
 //
@@ -9,11 +9,38 @@ import UIKit
 
 class EventViewController: UIViewController {
 
+    
+    @IBOutlet weak var CardBtn: UIButton!
+    
+    @IBOutlet weak var CashBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Initial button states
+        CardBtn.isSelected = false
+        CashBtn.isSelected = false
+                
+                // Set default images
+        CardBtn.setImage(UIImage(systemName: "circle"), for: .normal)
+        CardBtn.setImage(UIImage(systemName: "checkmark.circle.fill"),for: .selected)
+                
+        CashBtn.setImage(UIImage(systemName: "circle"), for: .normal)
+        CashBtn.setImage(UIImage(systemName: "checkmark.circle.fill"),for: .selected)
      
     }
+    @IBAction func PaymentTapped(_ sender: UIButton) {
+        if sender == CardBtn {
+            // Select Card, Deselect Cash
+            CardBtn.isSelected = true
+            CashBtn.isSelected = false
+        } else if sender == CashBtn {
+            // Select Cash, Deselect Card
+            CardBtn.isSelected = false
+            CashBtn.isSelected = true
+        }
+    }
+    
+   
     
 
    
