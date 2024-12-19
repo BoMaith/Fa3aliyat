4da2030d5a3482f0e4d8a4299e8cbc61e4b93c22
yassfee <yassfee923@gmail.com>
2024-12-19 16:50:29 +0300

    nice

---
+++ Fa3aliyat/Yousif/HistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryTableViewCell.swift
+//  Fa3aliyat
+//
+//  Created by BP-36-224-15 on 19/12/2024.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Fa3aliyat/Yousif/PayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PayViewController.swift
+//  Fa3aliyat
+//
+//  Created by BP-36-224-15 on 19/12/2024.
+//
+
+import UIKit
+
+class PayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
