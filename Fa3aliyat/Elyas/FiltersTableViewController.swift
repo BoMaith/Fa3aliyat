61aca7fc283e38af76dcaff94ad08fb729abecc7
RidhaQambar <ridah0030@gmail.com>
2024-12-25 06:56:40 -0800

    working on filter page

--- Fa3aliyat/Elyas/FiltersTableViewController.swift
+++ Fa3aliyat/Elyas/FiltersTableViewController.swift
@@ -3,85 +3,197 @@ import UIKit
 class FiltersTableViewController: UITableViewController {
 
     @IBOutlet weak var categoryButton: UIButton!
+    @IBOutlet weak var dateTimePicker: UIDatePicker!
+    @IBOutlet weak var locationTextField: UITextField!
     
-    var selectedRows: Set<IndexPath> = []
+    var selectedPriceRow: IndexPath? // Tracks the selected row for "Filter by Price"
+    var selectedAgeRow: IndexPath?   // Tracks the selected row for "Filter by Age"
     var selectedButtonLabel: String? // Property to receive the selected row's label
+    
+    var selectedDate: Date?  // Stores selected Date (from a DatePicker), initially nil
+    var selectedTime: Date?  // Stores selected Time (from a TimePicker), initially nil
+    var selectedLocation: String?  // Stores selected location (text input)
+    
+    var selectedFilters: [Any?] = [nil, nil, nil, nil, nil] // [category, dateAndTime, price, location, age]
 
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView is called") // See if this is ever triggered.
+    }
+    
     override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        print("Delegate and DataSource connected: \(tableView.delegate != nil && tableView.dataSource != nil)")
+
+        
         super.viewDidLoad()
+        self.tableView.setNeedsLayout()
+        self.tableView.layoutIfNeeded()
+        print("Table view loaded")
 
-        // Ensure ticks are hidden initially
-        tableView.reloadData()
         self.tabBarController?.tabBar.isHidden = true
+        clearAllSelections()
 
-        // Retrieve the saved filter from UserDefaults if available
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+        
         if let savedCategory = UserDefaults.standard.string(forKey: "selectedCategory") {
             categoryButton.setTitle(savedCategory, for: .normal)
             selectedButtonLabel = savedCategory
+            selectedFilters[0] = savedCategory // Store category in selectedFilters[0]
         } else {
-            // If there's no saved category, show default title
             categoryButton.setTitle("Select Category", for: .normal)
+            selectedFilters[0] = "Select Category" // Default value
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
-        // If the label is not set (this means we're not coming from Categories), set it to "Select Category"
-        if selectedButtonLabel == nil {
-            categoryButton.setTitle("Select Category", for: .normal)
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveFilters))
+        navigationItem.rightBarButtonItem = saveButton
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    @objc func saveFilters() {
+        
+        // Check if the category has been changed from "Select Category"
+        if let categoryTitle = categoryButton.titleLabel?.text, categoryTitle != "Select Category" {
+            selectedFilters[0] = categoryTitle
+            print("Category selected: \(categoryTitle)")
         } else {
-            categoryButton.setTitle(selectedButtonLabel, for: .normal)
+            selectedFilters[0] = nil // Do not save category if still "Select Category"
+            print("Category not selected or still default.")
+        }
+        
+        // Save Date and Time as a combined string
+        if let dateAndTime = selectedFilters[1] as? String {
+            UserDefaults.standard.set(dateAndTime, forKey: "selectedDateAndTime") // Save combined date and time
+        }
+
+        // Save Price
+        if let price = selectedFilters[2] as? String {
+            UserDefaults.standard.set(price, forKey: "selectedPrice")
+        }
+
+        // Save Location
+        if let location = selectedFilters[3] as? String {
+            UserDefaults.standard.set(location, forKey: "selectedLocation")
+        }
+
+        // Save Age
+        if let age = selectedFilters[4] as? String {
+            UserDefaults.standard.set(age, forKey: "selectedAge")
+        }
+
+        print("Filters saved: \(selectedFilters)")
+    }
+
+    // Handle Date and Time Selection
+    @IBAction func dateTimeSelected(_ sender: UIDatePicker) {
+        selectedDate = sender.date
+        selectedTime = sender.date // Since we have one picker for both date and time
+        updateSelectedFilters() // Update filters array when date and time are selected
+    }
+
+    @IBAction func locationChanged(_ sender: UITextField) {
+        if let text = sender.text {
+            selectedLocation = text
+            selectedFilters[3] = selectedLocation // Store location in selectedFilters[3]
         }
     }
 
-    // MARK: - Table View Data Source
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = super.tableView(tableView, cellForRowAt: indexPath)
-
-        // Hide tick initially
-        if let tickImageView = cell.contentView.subviews.first(where: { $0 is UIImageView }) as? UIImageView {
-            tickImageView.isHidden = !selectedRows.contains(indexPath)
+    // Update filters with current selections
+    private func updateSelectedFilters() {
+        if let date = selectedDate {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            selectedFilters[1] = dateFormatter.string(from: date)
         }
-
-        return cell
     }
 
-    // MARK: - Table View Delegate Methods
+    // MARK: - Table View Delegate Method for Row Selection (Price and Age)
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        print("Row selected: section \(indexPath.section), row \(indexPath.row)") // Add this to check if the method is called
 
-        // Toggle selection state
-        if selectedRows.contains(indexPath) {
-            selectedRows.remove(indexPath)
-            hideTickIcon(in: cell)
-        } else {
-            selectedRows.insert(indexPath)
-            showTickIcon(in: cell)
+        // Check if the row belongs to price section or age section
+        if indexPath.section == 2 { // Price section
+            handlePriceSelection(at: indexPath)
+        } else if indexPath.section == 4 { // Age section
+            handleAgeSelection(at: indexPath)
         }
-
-        // Deselect row to remove highlight effect
+        
+        // Deselect row to remove highlight
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    // MARK: - Helper Methods
-    private func showTickIcon(in cell: UITableViewCell) {
-        if let tickImageView = cell.contentView.subviews.first(where: { $0 is UIImageView }) as? UIImageView {
-            tickImageView.isHidden = false
+    // Handle Price Selection
+    private func handlePriceSelection(at indexPath: IndexPath) {
+        // If the same row is tapped again, deselect it
+        if selectedPriceRow == indexPath {
+            selectedPriceRow = nil
+            selectedFilters[2] = nil
+        } else {
+            // Deselect previously selected row
+            if let previouslySelectedIndexPath = selectedPriceRow {
+                if let cell = tableView.cellForRow(at: previouslySelectedIndexPath) {
+                    cell.accessoryType = .none
+                }
+            }
+            selectedPriceRow = indexPath
+            if let cell = tableView.cellForRow(at: indexPath) {
+                cell.accessoryType = .checkmark
+                selectedFilters[2] = cell.textLabel?.text // Store the label of the selected row
+            }
         }
+        
+        // Reload the row to make sure the accessory type is updated
+        tableView.reloadRows(at: [indexPath], with: .none)
     }
 
-    private func hideTickIcon(in cell: UITableViewCell) {
-        if let tickImageView = cell.contentView.subviews.first(where: { $0 is UIImageView }) as? UIImageView {
-            tickImageView.isHidden = true
+    // Handle Age Selection
+    private func handleAgeSelection(at indexPath: IndexPath) {
+        // If the same row is tapped again, deselect it
+        if selectedAgeRow == indexPath {
+            selectedAgeRow = nil
+            selectedFilters[4] = nil
+        } else {
+            // Deselect previously selected row
+            if let previouslySelectedIndexPath = selectedAgeRow {
+                if let cell = tableView.cellForRow(at: previouslySelectedIndexPath) {
+                    cell.accessoryType = .none
+                }
+            }
+            selectedAgeRow = indexPath
+            if let cell = tableView.cellForRow(at: indexPath) {
+                cell.accessoryType = .checkmark
+                selectedFilters[4] = cell.textLabel?.text // Store the label of the selected row
+            }
         }
+        
+        // Reload the row to make sure the accessory type is updated
+        tableView.reloadRows(at: [indexPath], with: .none)
     }
 
-    // MARK: - Save the selected filter when the user presses the category button
-    @IBAction func categoryButtonTapped(_ sender: UIButton) {
-        // When a category is selected, save it to UserDefaults
-        if let selectedCategory = categoryButton.title(for: .normal) {
-            UserDefaults.standard.set(selectedCategory, forKey: "selectedCategory")
+    // Clear all selections
+    private func clearAllSelections() {
+        selectedPriceRow = nil
+        selectedAgeRow = nil
+        selectedDate = nil
+        selectedTime = nil
+        selectedLocation = nil
+
+        // Clear checkmarks in price and age sections
+        for section in [2, 4] {
+            let numberOfRows = tableView.numberOfRows(inSection: section)
+            for row in 0..<numberOfRows {
+                if let cell = tableView.cellForRow(at: IndexPath(row: row, section: section)) {
+                    cell.accessoryType = .none
+                }
+            }
         }
     }
 }
